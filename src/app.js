fd0ab3aa7c12c17a2ae55e9d36f4200758c1a3d7
manas-agarwal16 <manas.agarwal1604@gmail.com>
2025-03-06 20:58:40 +0530

    / route html response

--- src/app.js
+++ src/app.js
@@ -11,7 +11,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.send(`
+        <h2>Hello Educase India Team</h2>
+        <p>To test the APIs, use the following endpoints:</p>
+        <ul>
+          <li><strong>Add a School:</strong> <br> 
+            <code>POST https://educase-assignment-1-bju1.onrender.com/addSchool</code>
+          </li>
+          <li><strong>List Schools Near a Location:</strong> <br> 
+            <code>GET https://educase-assignment-1-bju1.onrender.com/listSchools/:userLongitude/:userLatitude</code>
+          </li>
+        </ul>
+        <p>Replace <code>:userLongitude</code> and <code>:userLatitude</code> with actual coordinates.</p>
+      `);
 });
 
 // Routes
