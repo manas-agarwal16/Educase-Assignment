f16899663ef2c9a837a2df4ef357767d8bfba968
manas-agarwal16 <manas.agarwal1604@gmail.com>
2025-03-06 20:24:56 +0530

    route update

--- src/routes/school.routes.js
+++ src/routes/school.routes.js
@@ -4,6 +4,6 @@ import { AddSchool, ListSchools } from "../controllers/school.controller.js";
 const router = Router();
 
 router.route("/addSchool").post(AddSchool);
-router.route("/listSchool/:userLongitude/:userLatitude").get(ListSchools);
+router.route("/listSchools/:userLongitude/:userLatitude").get(ListSchools);
 
 export default router;
